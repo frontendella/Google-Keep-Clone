a6d283e0cb748a8f20d144ef65596aa5af477bc8
Ella R <ryukhinaella@gmail.com>
2023-01-26 21:01:55 -0800

    Event handlers are added

---
+++ app.js
@@ -0,0 +1,37 @@
+class App {
+    constructor() {
+        this.$form = document.querySelector('#form');
+        this.$noteTitle = document.querySelector('#note-title');
+        this.$formButtons = document.querySelector('#form-buttons');
+
+        this.addEventListeners();
+    }
+
+    addEventListeners() {
+        document.body.addEventListener('click', event => {
+            this.handleFormClick(event);
+        });
+    }
+
+    handleFormClick(event) {
+        const isFormClicked = this.$form.contains(event.target);
+        if (isFormClicked) {
+            this.openForm();
+        } else {
+            this.closeForm()
+        }
+    }
+
+    openForm() {
+        this.$form.classList.add('form-open');
+        this.$noteTitle.style.display = 'block';
+        this.$formButtons.style.display = 'block';
+    }
+    closeForm() {
+        this.$form.classList.remove('form-open');
+        this.$noteTitle.style.display = 'none';
+        this.$formButtons.style.display = 'none';
+    }
+}
+
+new App();
