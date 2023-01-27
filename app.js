a4813214ff1b06dd99fff54fa67fefbd3a94656c
Ella R <ryukhinaella@gmail.com>
2023-01-26 21:44:24 -0800

    Prevent default webpage refresh and add logic for "submit"

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 class App {
     constructor() {
+        this.notes = []
+
         this.$form = document.querySelector('#form');
         this.$noteTitle = document.querySelector('#note-title');
+        this.$noteText = document.querySelector('#note-text');
         this.$formButtons = document.querySelector('#form-buttons');
-
         this.addEventListeners();
     }
 
@@ -11,6 +13,16 @@ class App {
         document.body.addEventListener('click', event => {
             this.handleFormClick(event);
         });
+
+        this.$form.addEventListener('submit', event => {
+            event.preventDefault()
+            const title = this.$noteTitle.value;
+            const text = this.$noteText.value;
+            const hasNote = title || text;
+            if (hasNote) {
+                this.addNote({ title, text })
+            }
+        })
     }
 
     handleFormClick(event) {
@@ -31,6 +43,17 @@ class App {
         this.$form.classList.remove('form-open');
         this.$noteTitle.style.display = 'none';
         this.$formButtons.style.display = 'none';
+    }
+
+    addNote(note) {
+        const newNote = {
+            title: note.title,
+            text: note.text,
+            color: 'white',
+            id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+        };
+        this.notes = [...this.notes, newNote];
+        console.log(this.notes)
     }
 }
 
