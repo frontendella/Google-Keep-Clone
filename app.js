4c43eee67eefa44aa2e156df9f3d16881801149d
Ella R <ryukhinaella@gmail.com>
2023-01-27 18:40:16 -0800

    "Close" button functionality is added

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 class App {
     constructor() {
         this.notes = [];
-        this.$placeholder = document.querySelector('#placeholder');
+
+        this.$placeholder = document.querySelector("#placeholder");
         this.$form = document.querySelector("#form");
-        this.$notes = document.querySelector('#notes');
+        this.$notes = document.querySelector("#notes");
         this.$noteTitle = document.querySelector("#note-title");
         this.$noteText = document.querySelector("#note-text");
         this.$formButtons = document.querySelector("#form-buttons");
+        this.$formCloseButton = document.querySelector('#form-close-button');
         this.addEventListeners();
     }
 
@@ -25,14 +27,23 @@ class App {
                 this.addNote({ title, text });
             }
         });
+        this.$formCloseButton.addEventListener('click', (event) => {
+            event.stopPropagation()
+            this.closeForm()
+        })
 
     }
 
     handleFormClick(event) {
         const isFormClicked = this.$form.contains(event.target);
+        const title = this.$noteTitle.value;
+        const text = this.$noteText.value;
+        const hasNote = title || text;
 
         if (isFormClicked) {
             this.openForm();
+        } else if (hasNote) {
+            this.addNote({ title, text });
         } else {
             this.closeForm();
         }
@@ -48,29 +59,31 @@ class App {
         this.$form.classList.remove("form-open");
         this.$noteTitle.style.display = "none";
         this.$formButtons.style.display = "none";
-        this.$noteTitle.value = ''
-        this.$noteText.value = ''
+        this.$noteTitle.value = "";
+        this.$noteText.value = "";
     }
 
-    addNote(note) {
+    addNote({ title, text }) {
         const newNote = {
-            title: note.title,
-            text: note.text,
+            title,
+            text,
             color: "white",
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
         };
         this.notes = [...this.notes, newNote];
         this.displayNotes();
-        this.closeForm()
+        this.closeForm();
     }
 
     displayNotes() {
         const hasNotes = this.notes.length > 0;
-        this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
+        this.$placeholder.style.display = hasNotes ? "none" : "flex";
 
-        this.$notes.innerHTML = this.notes.map(note => `
-          <div style="background: ${note.color}" class="note">
-            <div class="${note.title && 'note-title'}">${note.title}</div>
+        this.$notes.innerHTML = this.notes
+            .map(
+                note => `
+          <div style="background: ${note.color};" class="note">
+            <div class="${note.title && "note-title"}">${note.title}</div>
             <div class="note-text">${note.text}</div>
             <div class="toolbar-container">
               <div class="toolbar">
@@ -79,7 +92,9 @@ class App {
               </div>
             </div>
           </div>
-       `).join("")
+       `
+            )
+            .join("");
     }
 }
 
